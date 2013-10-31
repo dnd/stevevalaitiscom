cc7aef6f5adf7ae9e8cac7a35187595ce3d69141
Steve Valaitis <steve@digitalnothing.com>
2013-10-31 14:19:20 -0500

    Add gh-pages deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SELF=`basename $0`
+SOURCE_BRANCH="master"
+DEST_BRANCH="gh-pages"
+DEPLOY_DIR="_deploy"
+
+git checkout $SOURCE_BRANCH
+jekyll build -d $TMP_DIR
+git checkout $DEST_BRANCH
+# This will remove previous files, which we may not want (e.g. CNAME)
+git rm -rf .
+git clean -xdf -e $DEPLOY_DIR
+cp -r $DEPLOY_DIR/. .
+# Delete this script from the output
+rm ./$SELF
+rm -r $DEPLOY_DIR
+git add -A
+git commit
+#git checkout $SOURCE_BRANCH
